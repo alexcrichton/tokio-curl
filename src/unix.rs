5dea09b2a46f848becb81148df410e4e7545a36e
Alex Crichton <alex@alexcrichton.com>
2017-01-22 12:15:03 -0800

    Don't assume fds are open when removing
    According to syscall traces from #12 it looks like curl may close file
    descriptors before calling our socket callback, so we can't call
    `deregister` on them as it may return EBADF. Now we added this logic in
    3306b320 but now I can't recall why... Let's see what tests fail.

--- src/unix.rs
+++ src/unix.rs
@@ -230,9 +230,7 @@ impl Data {
         if events.remove() {
             assert!(token > 0);
             debug!("remove socket: {} / {}", socket, token - 1);
-            state.sockets.remove(token - 1).unwrap()
-                 .stream.deregister(&self.handle)
-                 .expect("failed to deregister");
+            state.sockets.remove(token - 1).unwrap();
             return
         }
 
